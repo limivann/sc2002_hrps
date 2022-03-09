198d9d1386ccff16e1f17439e8731c9429bb3b30
lordAaron0121 <88924766+lordAaron0121@users.noreply.github.com>
2022-03-09 13:23:15 +0800

    guest ver 1

---
+++ src/Guest.java
@@ -0,0 +1,37 @@
+public class Guest {
+    enum Gender {Male, Female}
+
+    String name;
+    String creditCard;
+    String address;
+    String country;
+    Gender gender;
+    String identity;
+    String nationality;
+    String contact;
+    // Payment paymentDetails;
+    // Reservation reservationDetails;
+    public Guest(String name, String creditCard, String address, String country, Gender gender, String identity, String nationality, String contact){
+        this.name = name;
+        this.creditCard = creditCard;
+        this.address = address;
+        this.country = country;
+        this.gender = gender;
+        this.identity = identity;
+        this.nationality = nationality;
+        this.contact = contact;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getCreditCard() {
+        return creditCard;
+    }
+    public String getContact() {
+        return contact;
+    }
+    //make Reservation 
+    public void printGuestDetails(){
+        
+    }
+}
