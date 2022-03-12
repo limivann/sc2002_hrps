7973844fc372569d075189713e4dcd3ddbce791b
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2022-03-12 22:37:37 +0800

    merged kaichen's file for guest and room

--- src/Room.java
+++ src/Room.java
@@ -3,44 +3,90 @@ package src;
 public class Room {
 	enum RoomStatus {VACANT, OCCUPIED, RESERVED, UNDER_MAINTENANCE};
 	enum RoomType {SINGLE, DOUBLE, DELUXE, VIP_SUITE}
-	RoomType type;
-	int floorNumber;
-	int roomNumber;
-	String roomNumberString;
-	RoomStatus roomStatus;
-	double price;
-	boolean isWifiEnabled;
-	boolean isSmokingAllowed;
-	String guestName;
+	private RoomType type;
+	private int floorNumber;
+	private int roomNumber;
+	private String roomNumberString;
+	private RoomStatus roomStatus;
+	private double price;
+	private boolean isWifiEnabled;
+	private boolean isSmokingAllowed;
+	private String guestName;
 	
-	Room(RoomType type, int floorNumber, int roomNumber, RoomStatus roomStatus, double price, boolean isWifiEnabled, boolean isSmokingAllowed) {
+	
+	/**
+	 * The constructor of a Room
+	 * @param type The type of the room
+	 * @param floorNumber The floor number of the room
+	 * @param roomNumber The room number of the room
+	 * @param roomStatus the status of the room
+	 * @param price The price of the room
+	 * @param isWifiEnabled whether the room has wifi or not
+	 * @param isSmokingAllowed whether you can smoke in the room
+	 * @see RoomType
+	 * @see RoomStatus
+	 */
+	public Room(RoomType type, int floorNumber, int roomNumber, RoomStatus roomStatus, double price, boolean isWifiEnabled, boolean isSmokingAllowed) {
 		this.type = type;
+		this.floorNumber = floorNumber;
+		this.roomNumber = roomNumber;
 		this.roomNumberString = floorNumber+"-"+roomNumber;
 		this.roomStatus = roomStatus;
 		this.price = price;
 		this.isWifiEnabled = isWifiEnabled;
 		this.isSmokingAllowed = isSmokingAllowed;
 	}
-	
-	String getRoomNumberString() {
+
+	/**
+	 * Getter method of room number
+	 * @return number of the room
+	 */
+	public String getRoomNumberString() {
 		return this.roomNumberString;
 	}
-	void setRoomStatus(RoomStatus roomStatus) {
+	/**
+	 * Setter method for room status
+	 * @param roomStatus Occupied or not
+	 * @see RoomStatus
+	 */
+	public void setRoomStatus(RoomStatus roomStatus) {
 		this.roomStatus = roomStatus;
 	}
-	RoomStatus getRoomStatus() {
+	/**
+	 * Getter method of room status
+	 * @return status of the room
+	 */
+	public RoomStatus getRoomStatus() {
 		return this.roomStatus;
 	}
-	double getPrice() {
+
+	/**
+	 * Getter method of room price
+	 * @return price of the room
+	 */
+	public double getPrice() {
 		return this.price;
 	}
-	String getGuestName() {
+	/**
+	 * Getter method of guest's name
+	 * @return guest name of the room
+	 */
+	public String getGuestName() {
 		return guestName;
 	}
-	void setGuestName(String guestName) {
+	/**
+	 * Setter for guest name
+	 * @param guestName The name of the guest in the room
+	 */
+	public void setGuestName(String guestName) {
 		this.guestName = guestName;
 	}
-	void printRoomStatus(RoomStatus roomStatus) {
+
+	/**
+	 * Print out the room status of the room
+	 * @param roomStatus the status of the room
+	 */
+	public void printRoomStatus() {
 		switch(roomStatus) {
 		case VACANT:
 			System.out.println("Vacant");
@@ -56,4 +102,40 @@ public class Room {
 			break;
 		}
 	}
+
+	/**
+	 * This is the function that print
+	 * the room type of the room
+	 */
+	public void printRoomtype(){
+		switch(type){
+			case SINGLE:
+			System.out.println("Type: Single Room");
+			break;
+			case DOUBLE:
+			System.out.println("Type: Double Room");
+			break;
+			case DELUXE:
+			System.out.println("Type: Deluxe Room");
+			case VIP_SUITE:
+			System.out.println("Type: VIP Suite");
+		}
+	}
+	/**
+	 * This is the function that print out the room detail
+	 */
+	public void printRoom(){
+		System.out.println("----------------");
+        System.out.printf("Room number: %s\n", roomNumberString);
+		System.out.printf("Room status: %s\n", roomStatus);
+		if (roomStatus == RoomStatus.OCCUPIED || roomStatus == RoomStatus.RESERVED) {
+			System.out.printf("Guest Name: %s\n", guestName);
+		}
+		System.out.printf("Room price: %s\n", price);
+		System.out.printf("Wifi Enabled: %s\n", isWifiEnabled);
+		System.out.printf("Smoking Allowed: %s\n", isSmokingAllowed);
+        System.out.println("----------------");
+	}
+
+
 }
