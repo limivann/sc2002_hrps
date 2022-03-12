1eafcb7f743b346d35593f577f55cc4cce7e9a44
HillSeahWQ <99699704+HillSeahWQ@users.noreply.github.com>
2022-03-12 17:13:17 +0800

    Add files via upload

---
+++ src/MenuItem_hill.java
@@ -0,0 +1,26 @@
+package programming.blackbull;
+
+public class MenuItem {
+
+    private String name;
+    private String description;
+    private double price;
+
+    public MenuItem(String name, String description, double price){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
