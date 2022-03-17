e97adff3b7a287d58a4d954983fdc061e99cb65c
lordAaron0121 <88924766+lordAaron0121@users.noreply.github.com>
2022-03-17 17:52:02 +0800

    room V1

---
+++ src/model/Room.java
@@ -0,0 +1,149 @@
+package src.model;
+
+// import java.util.ArrayList;
+
+public class Room {
+	public enum RoomStatus {VACANT, OCCUPIED, RESERVED, UNDER_MAINTENANCE};
+	public enum RoomType {SINGLE, DOUBLE, DELUXE, VIP_SUITE}
+	private RoomType type;
+	private int floorNumber;
+	private int roomNumber;
+	private String roomNumberString;
+	private RoomStatus roomStatus;
+	private double price;
+	private boolean isWifiEnabled;
+	private boolean isSmokingAllowed;
+	private String guestName;
+	// private ArrayList<OrderItem> orders;
+	
+	/**
+	 * The constructor of a Room
+	 * @param type The type of the room
+	 * @param floorNumber The floor number of the room
+	 * @param roomNumber The room number of the room
+	 * @param roomStatus the status of the room
+	 * @param price The price of the room
+	 * @param isWifiEnabled whether the room has wifi or not
+	 * @param isSmokingAllowed whether you can smoke in the room
+	 * @see RoomType
+	 * @see RoomStatus
+	 */
+	public Room(RoomType type, int floorNumber, int roomNumber, RoomStatus roomStatus, double price, boolean isWifiEnabled, boolean isSmokingAllowed) {
+		this.type = type;
+		this.floorNumber = floorNumber;
+		this.roomNumber = roomNumber;
+		this.roomNumberString = floorNumber+"-"+roomNumber;
+		this.roomStatus = roomStatus;
+		this.price = price;
+		this.isWifiEnabled = isWifiEnabled;
+		this.isSmokingAllowed = isSmokingAllowed;
+		// this.orders = new ArrayList<OrderItem>();
+	}
+
+	/**
+	 * Getter method of room number
+	 * @return number of the room
+	 */
+	public String getRoomNumberString() {
+		return this.roomNumberString;
+	}
+	/**
+	 * Setter method for room status
+	 * @param roomStatus Occupied or not
+	 * @see RoomStatus
+	 */
+	public void setRoomStatus(RoomStatus roomStatus) {
+		this.roomStatus = roomStatus;
+	}
+	/**
+	 * Getter method of room status
+	 * @return status of the room
+	 */
+	public RoomStatus getRoomStatus() {
+		return this.roomStatus;
+	}
+
+	/**
+	 * Getter method of room price
+	 * @return price of the room
+	 */
+	public double getPrice() {
+		return this.price;
+	}
+	/**
+	 * Setter for guest name
+	 * @param guestName The name of the guest in the room
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	/**
+	 * Getter method of guest's name
+	 * @return guest name of the room
+	 */
+	public String getGuestName() {
+		return guestName;
+	}
+	/**
+	 * Setter for guest name
+	 * @param guestName The name of the guest in the room
+	 */
+	public void setGuestName(String guestName) {
+		this.guestName = guestName;
+	}
+
+	/**
+	 * Print out the room status of the room
+	 * @param roomStatus the status of the room
+	 */
+	public void printRoomStatus() {
+		switch(roomStatus) {
+		case VACANT:
+			System.out.println("Vacant");
+			break;
+		case OCCUPIED:
+			System.out.println("Occupied");
+			break;
+		case RESERVED:
+			System.out.println("Reserved");
+			break;
+		case UNDER_MAINTENANCE:
+			System.out.println("Under UNDER_MAINTENANCE");
+			break;
+		}
+	}
+
+	/**
+	 * This is the function that print
+	 * the room type of the room
+	 */
+	public void printRoomtype(){
+		switch(type){
+			case SINGLE:
+			System.out.println("Type: Single Room");
+			break;
+			case DOUBLE:
+			System.out.println("Type: Double Room");
+			break;
+			case DELUXE:
+			System.out.println("Type: Deluxe Room");
+			case VIP_SUITE:
+			System.out.println("Type: VIP Suite");
+		}
+	}
+	/**
+	 * This is the function that print out the room detail
+	 */
+	public void printRoom(){
+		System.out.println("----------------");
+        System.out.printf("Room number: %s\n", roomNumberString);
+		System.out.printf("Room status: %s\n", roomStatus);
+		if (roomStatus == RoomStatus.OCCUPIED || roomStatus == RoomStatus.RESERVED) {
+			System.out.printf("Guest Name: %s\n", guestName);
+		}
+		System.out.printf("Room price: %s\n", price);
+		System.out.printf("Wifi Enabled: %s\n", isWifiEnabled);
+		System.out.printf("Smoking Allowed: %s\n", isSmokingAllowed);
+        System.out.println("----------------");
+	}
+}
---
+++ src/view/RoomView.java
@@ -0,0 +1,134 @@
+package src.view;
+import java.util.Scanner;
+import src.model.Room;
+import src.controller.RoomManager;
+
+public class RoomView extends MainView{
+    private RoomManager a = new RoomManager();   
+    public void printMenu(){
+        System.out.println("\n1. Create room");
+        System.out.println("2. Print room");
+        System.out.println("3. Update room");
+        System.out.println("4. Print room status");
+        System.out.println("5. Remove room");
+        System.out.println("6. Exit");
+    }
+
+    @Override
+    public void viewapp() {
+        Scanner sc = new Scanner(System.in);
+        printMenu();
+        int opt = sc.nextInt();
+        do{
+            switch (opt){
+                case 1:
+                createRoom();
+                break;
+                case 2:
+                printRoom();
+                break;
+                case 3:
+                updateRoom();
+                break;
+                case 4:
+                printRoomStatus();
+                break;
+                case 5:
+                removeRoom();
+                break;
+            }
+            printMenu();
+            opt = sc.nextInt();
+        }while (opt < 6);
+    }
+    public void createRoom(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the floor number: ");
+        int floor = sc.nextInt();
+        System.out.println("Enter the room number: ");
+        int room = sc.nextInt();
+        System.out.println("Enter the price: ");
+        double price = sc.nextDouble();
+        System.out.println("Is this room wifi-enabled?");
+        boolean wifi = sc.nextBoolean();
+        System.out.println("Is smoking allowed in this room?");
+        boolean smoking = sc.nextBoolean();
+
+        System.out.println("Enter choice of room type: ");
+        System.out.println("1 for Single room");
+        System.out.println("2 for Double room");
+        System.out.println("3 for Deluxe room");
+        System.out.println("4 for VIP suite");
+        int opt = sc.nextInt();
+        
+        switch (opt){
+            case 1:
+            a.create(Room.RoomType.SINGLE, floor, room,  price, wifi, smoking);
+            break;
+            case 2:
+            a.create(Room.RoomType.DOUBLE, floor, room,  price, wifi, smoking);
+            break;
+            case 3:
+            a.create(Room.RoomType.DELUXE, floor, room,  price, wifi, smoking);
+            break;
+            case 4:
+            a.create(Room.RoomType.VIP_SUITE, floor, room,  price, wifi, smoking);
+            break;
+        }     
+    }
+    public void printRoom(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the floor: ");
+        int floor = sc.nextInt();
+        System.out.println("Enter the room: ");
+        int room = sc.nextInt();
+        a.printRoom(floor, room);
+    }
+    public void updateRoom(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Which room do you want to update?");
+        System.out.println("Enter floor:");
+        int floor = sc.nextInt();
+        System.out.println("Enter room:");
+        int room = sc.nextInt();
+
+        System.out.println("What do you wish to update?");
+        System.out.println("1 for status");
+        System.out.println("2 for price");
+        int opt = sc.nextInt();
+        if (opt == 1){
+            System.out.println("Which is the new status?");
+            System.out.println("1 for vacant");
+            System.out.println("2 for occupied");
+            System.out.println("3 for reserved");
+            System.out.println("4 for under maintenance");
+            int status = sc.nextInt();
+            a.updateStatus(floor, room, status);
+        }else if (opt == 2){
+            System.out.println("Enter the new price?");
+            double price = sc.nextDouble();
+            a.updatePrice(floor, room, price);
+        }
+    }
+    public void removeRoom(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter floor:");
+        int floor = sc.nextInt();
+        System.out.println("Enter room:");
+        int room = sc.nextInt();
+        a.remove(floor, room);
+    }
+    public void printRoomStatus(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter floor:");
+        int floor = sc.nextInt();
+        System.out.println("Enter room:");
+        int room = sc.nextInt();
+        a.printStatus(floor, room);
+    }
+}
