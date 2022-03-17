7da18a87b71b685f902b96dc257851e90fa84983
lordAaron0121 <88924766+lordAaron0121@users.noreply.github.com>
2022-03-17 23:58:43 +0800

    test

--- src/controller/RoomManager.java
+++ src/controller/RoomManager.java
@@ -1,46 +1,82 @@
 package src.controller;
 import java.util.HashMap;
-import java.util.Scanner;
 import src.model.Room;
 public class RoomManager{
     HashMap<String, Room> RoomList = new HashMap<String, Room>();
 
     public void create(Room.RoomType type, int floor, int room, double price, boolean wifi, boolean smoking) {
-        String id = floor+"-"+room;
-        Room newRoom = new Room(type, floor, room, Room.RoomStatus.VACANT, price, wifi, smoking);
-        RoomList.put(id, newRoom);
+        // Room target = searchRoom(floor, room);
+        // if (target.getRoomStatus() == Room.RoomStatus.VACANT){
+            String id = floor+"-"+room;
+            if (!RoomList.containsKey(id)){
+                Room newRoom = new Room(type, floor, room, Room.RoomStatus.VACANT, price, wifi, smoking);
+                RoomList.put(id, newRoom);
+                System.out.println("Room created successfully.");
+            }else{
+                System.out.println("Room ID already exists.");
+            }
+           
+        // }else {
+        //     // Add guest/reservation to waitlist
+        // }
     }
 
     public void updatePrice(int floor, int room, double price){
-        Room target = searchRoom(floor, room);
-        target.setPrice(price);
+        String id = floor+"-"+room;
+        if (RoomList.containsKey(id)){
+            Room target = searchRoom(floor, room);
+            target.setPrice(price);
+            System.out.println("Price updated successfully.");
+        }else{
+            System.out.println("Room id doesn't exists.");
+        }
+        
     }
 
     public void updateStatus(int floor, int room, int status){
-        Room target = searchRoom(floor, room);
-        if (status == 1){
-            target.setRoomStatus(Room.RoomStatus.VACANT);
-        }else if (status == 2){
-            target.setRoomStatus(Room.RoomStatus.OCCUPIED);
-        }else if (status == 3){
-            target.setRoomStatus(Room.RoomStatus.RESERVED);
-        }else if (status == 4){
-            target.setRoomStatus(Room.RoomStatus.UNDER_MAINTENANCE);
+        String id = floor+"-"+room;
+        if (RoomList.containsKey(id)){
+            Room target = searchRoom(floor, room);
+            if (status == 1){
+                target.setRoomStatus(Room.RoomStatus.VACANT);
+            }else if (status == 2){
+                target.setRoomStatus(Room.RoomStatus.OCCUPIED);
+            }else if (status == 3){
+                target.setRoomStatus(Room.RoomStatus.RESERVED);
+            }else if (status == 4){
+                target.setRoomStatus(Room.RoomStatus.UNDER_MAINTENANCE);
+            }    
+            System.out.println("Status updated successfully.");        
+        }else{
+            System.out.println("Room id doesn't exists. ");
         }
+
     }
 
     public void remove(int floor, int room) {
         String id = floor+"-"+room;
-        RoomList.remove(id);
+        if (RoomList.containsKey(id)){
+            RoomList.remove(id);
+            System.out.println("Room removed successfully.");
+        }else{
+            System.out.println("Room doesn't exists.");
+        }
+        
     }
 
     public Room searchRoom(int floor, int room){
         String id = floor+"-"+room;
         return RoomList.get(id);
     }
-    public void  printRoom(int floor, int room){
-        Room target = searchRoom(floor, room);
-        target.printRoom();
+    
+    public void printRoom(int floor, int room){
+        String id = floor+"-"+room;
+        if (RoomList.containsKey(id)){
+            Room target = searchRoom(floor, room);
+            target.printRoom();            
+        }else{
+            System.out.println("Room doesn't exists.");
+        }
     }
     
     public void printStatus(int floor, int room){
--- src/view/RoomView.java
+++ src/view/RoomView.java
@@ -2,6 +2,7 @@ package src.view;
 import java.util.Scanner;
 import src.model.Room;
 import src.controller.RoomManager;
+import java.util.InputMismatchException;
 
 public class RoomView extends MainView{
     private RoomManager a = new RoomManager();   
@@ -42,93 +43,171 @@ public class RoomView extends MainView{
         }while (opt < 6);
     }
     public void createRoom(){
+        int floor = 0, room = 0, opt = 0;
+        double price = 0;
+        boolean wifi = true, smoking = false;
+
         Scanner sc = new Scanner(System.in);
+        do{
+            try{
+                System.out.println("Enter the floor number (integer): ");
+                floor = sc.nextInt();
+                System.out.println("Enter the room number (integer): ");
+                room = sc.nextInt();
+                System.out.println("Enter the price (double): ");
+                price = sc.nextDouble();  
+                System.out.println("Is this room wifi-enabled? (type true or false)");
+                wifi = sc.nextBoolean();
+                System.out.println("Is smoking allowed in this room? (type true or false)");
+                smoking = sc.nextBoolean();          
+            }catch (InputMismatchException e){
+                System.out.println("Wrong data type! ");
+                System.out.println("________________\n");
+            }
+            sc.nextLine();
+        }while (floor <= 0 || room <= 0 || price <= 0);
 
-        System.out.println("Enter the floor number: ");
-        int floor = sc.nextInt();
-        System.out.println("Enter the room number: ");
-        int room = sc.nextInt();
-        System.out.println("Enter the price: ");
-        double price = sc.nextDouble();
-        System.out.println("Is this room wifi-enabled?");
-        boolean wifi = sc.nextBoolean();
-        System.out.println("Is smoking allowed in this room?");
-        boolean smoking = sc.nextBoolean();
+        do{
+            try{
+                System.out.println("Enter choice of room type: ");
+                System.out.println("1 for Single room");
+                System.out.println("2 for Double room");
+                System.out.println("3 for Deluxe room");
+                System.out.println("4 for VIP suite");
+                opt = sc.nextInt();            
+                switch (opt){
+                    case 1:
+                    a.create(Room.RoomType.SINGLE, floor, room,  price, wifi, smoking);
+                    break;
+                    case 2:
+                    a.create(Room.RoomType.DOUBLE, floor, room,  price, wifi, smoking);
+                    break;
+                    case 3:
+                    a.create(Room.RoomType.DELUXE, floor, room,  price, wifi, smoking);
+                    break;
+                    case 4:
+                    a.create(Room.RoomType.VIP_SUITE, floor, room,  price, wifi, smoking);
+                    break;
+                    default: 
+                    System.out.println("Please enter number from 1 to 4 only.");
+                    break;
+                }                    
+            }catch (InputMismatchException e){
+                System.out.println("Wrong data type!");
+            }
+             sc.nextLine();
+        }while (opt < 1 && opt > 4);
 
-        System.out.println("Enter choice of room type: ");
-        System.out.println("1 for Single room");
-        System.out.println("2 for Double room");
-        System.out.println("3 for Deluxe room");
-        System.out.println("4 for VIP suite");
-        int opt = sc.nextInt();
-        
-        switch (opt){
-            case 1:
-            a.create(Room.RoomType.SINGLE, floor, room,  price, wifi, smoking);
-            break;
-            case 2:
-            a.create(Room.RoomType.DOUBLE, floor, room,  price, wifi, smoking);
-            break;
-            case 3:
-            a.create(Room.RoomType.DELUXE, floor, room,  price, wifi, smoking);
-            break;
-            case 4:
-            a.create(Room.RoomType.VIP_SUITE, floor, room,  price, wifi, smoking);
-            break;
-        }     
     }
+    private boolean isBoolean(String str) {
+        if (str.equalsIgnoreCase("true") || str.equalsIgnoreCase("false")) {
+            return true;
+        }
+        return false;
+    }
+
     public void printRoom(){
         Scanner sc = new Scanner(System.in);
+        int floor = 0, room = 0;
+        do{
+            try{
+                System.out.println("Enter the floor: ");
+                floor = sc.nextInt();
+                System.out.println("Enter the room: ");
+                room = sc.nextInt();
+                a.printRoom(floor, room);                
+            }catch (InputMismatchException e){
+                System.out.println("Wrong data type!");
+            }
+            sc.nextLine();
+        }while (floor <= 0 || room <= 0);
 
-        System.out.println("Enter the floor: ");
-        int floor = sc.nextInt();
-        System.out.println("Enter the room: ");
-        int room = sc.nextInt();
-        a.printRoom(floor, room);
     }
     public void updateRoom(){
         Scanner sc = new Scanner(System.in);
+        int floor = 0, room = 0, status = 0, opt = 0;
+        double price = 0;
 
-        System.out.println("Which room do you want to update?");
-        System.out.println("Enter floor:");
-        int floor = sc.nextInt();
-        System.out.println("Enter room:");
-        int room = sc.nextInt();
-
-        System.out.println("What do you wish to update?");
-        System.out.println("1 for status");
-        System.out.println("2 for price");
-        int opt = sc.nextInt();
-        if (opt == 1){
-            System.out.println("Which is the new status?");
-            System.out.println("1 for vacant");
-            System.out.println("2 for occupied");
-            System.out.println("3 for reserved");
-            System.out.println("4 for under maintenance");
-            int status = sc.nextInt();
-            a.updateStatus(floor, room, status);
-        }else if (opt == 2){
-            System.out.println("Enter the new price?");
-            double price = sc.nextDouble();
-            a.updatePrice(floor, room, price);
-        }
+        do {
+            try{
+                System.out.println("Which room do you want to update?");
+                System.out.println("Enter floor:");
+                floor = sc.nextInt();
+                System.out.println("Enter room:");
+                room = sc.nextInt();
+                System.out.println("What do you wish to update?");
+                do{
+                    System.out.println("1 for status");
+                    System.out.println("2 for price");
+                    opt = sc.nextInt();       
+                    switch (opt){
+                        case 1:
+                            status = -1; //temp value is -1
+                            do{
+                                System.out.println("Which is the new status?");
+                                System.out.println("1 for vacant");
+                                System.out.println("2 for occupied");
+                                System.out.println("3 for reserved");
+                                System.out.println("4 for under maintenance");
+                                if (status <= 4 && status >= 1){
+                                    status = sc.nextInt();
+                                }else{
+                                    System.out.println("Please enter number from 1 to 4 only.");
+                                }
+                            }while (status > 4 && status < 1);
+                            a.updateStatus(floor, room, status);  
+                            break;
+                        case 2:
+                            System.out.println("Enter the new price?");
+                            price = sc.nextDouble();
+                            a.updatePrice(floor, room, price);                
+                            break;
+                        default:
+                            System.out.println("Please enter number from 1 or 2 only.");  
+                            break;                
+                    }     
+                }while (opt < 1 && opt > 2);
+            }catch (InputMismatchException e){
+                System.out.println("Wrong data type!");
+            }
+            sc.nextLine();
+        }while (floor <= 0 || room <= 0 || status <= 0 || status > 4 || price <= 0);
+        
     }
+
     public void removeRoom(){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter floor:");
-        int floor = sc.nextInt();
-        System.out.println("Enter room:");
-        int room = sc.nextInt();
-        a.remove(floor, room);
+        int floor = 0, room = 0;
+        do{
+            try{
+                System.out.println("Enter the floor: ");
+                floor = sc.nextInt();
+                System.out.println("Enter the room: ");
+                room = sc.nextInt();
+                a.remove(floor, room);                
+            }catch (InputMismatchException e){
+                System.out.println("Wrong data type!");
+            }
+            sc.nextLine();
+        }while (floor <= 0 || room <= 0);
     }
+
     public void printRoomStatus(){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter floor:");
-        int floor = sc.nextInt();
-        System.out.println("Enter room:");
-        int room = sc.nextInt();
-        a.printStatus(floor, room);
+        int floor = 0, room = 0;
+        do{
+            try{
+                System.out.println("Enter the floor: ");
+                floor = sc.nextInt();
+                System.out.println("Enter the room: ");
+                room = sc.nextInt();
+                a.printStatus(floor, room);                
+            }catch (InputMismatchException e){
+                System.out.println("Wrong data type!");
+            }
+            sc.nextLine();
+        }while (floor <= 0 || room <= 0);
     }
 }
