6c9c9ee4572c7275df64a20e32cf0b9c3a9dfd12
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2022-03-19 00:20:56 +0800

    database test

--- src/Database.java
+++ src/Database.java
@@ -13,25 +13,34 @@ import src.model.*;
 public class Database {
     private static final String folder = "data";
     protected static HashMap<String, Guest> GUESTS = new HashMap<String, Guest>();
-    protected static HashMap<String, Guest> ROOMS = new HashMap<String, Guest>();
+    protected static HashMap<String, Room> ROOMS = new HashMap<String, Room>();
+    protected static HashMap<String, Reservation> RESERVATIONS = new HashMap<String, Reservation>();
+    protected static HashMap<String, Invoice> INVOICES = new HashMap<String, Invoice>();
     
     public Database() {
-        readSerializedObject(FileType.GUESTS);
+        if (!readSerializedObject(FileType.GUESTS)){
+            System.out.println("Read into Guest failed!");
+        }
+        
     }
     public static boolean readSerializedObject(FileType fileType) {
         String fileExtension = ".dat";
-        String filePath = "./" + folder + "/" + fileType.fileName + fileExtension;
+        String filePath = "./src/" + folder + "/" + fileType.fileName + fileExtension;
         try{
             FileInputStream fileInputStream = new FileInputStream(filePath);
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
             Object object = objectInputStream.readObject();
-            if (object != null && fileType == FileType.GUESTS) {
-                if (!(object instanceof HashMap)) {
-                    objectInputStream.close();
-                    return false;
-                }
-                GUESTS = (HashMap<String, Guest>) object;
+            if (!(object instanceof HashMap)) {
+                objectInputStream.close();
+                return false;
             }
+            // Read into database
+            if (fileType == FileType.GUESTS) {
+                GUESTS = (HashMap<String, Guest>) object;
+            } else if (fileType == FileType.ROOMS) {
+                ROOMS = (HashMap<String, Room>) object;
+            }
+
             objectInputStream.close();
             fileInputStream.close();
         } catch (IOException err) {
@@ -49,7 +58,7 @@ public class Database {
     
     public static boolean writeSerializedObject(FileType fileType) {
         String fileExtension = ".dat";
-        String filePath = "./" + folder + "/" + fileType.fileName + fileExtension;
+        String filePath = "./src/" + folder + "/" + fileType.fileName + fileExtension;
         try {
             FileOutputStream fileOutputStream = new FileOutputStream(filePath);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
@@ -63,5 +72,23 @@ public class Database {
             System.out.println("Error: " + err.getMessage());
             return false;
         }
+    }
+
+    private static boolean createDummyData() {
+        
+        return true;
+    }
+
+    public static boolean clearDatabase() {
+        // Initialize empty data
+        GUESTS = new HashMap<String, Guest>();
+        ROOMS = new HashMap<String, Room>();
+        writeSerializedObject(FileType.GUESTS);
+        writeSerializedObject(FileType.ROOMS);
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Database.clearDatabase();
     }
 }
